da80fceb0b06f942d2ffb0fcbe7e4623833754a9
David Valenzuela Guijosa <20243ds046@utez.edu.mx>
2025-05-21 08:16:37 -0600

    For para leer datos

--- calificacion/src/App.java
+++ calificacion/src/App.java
@@ -1,7 +1,28 @@
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner read = new Scanner(System.in);
+
+        float calificacion, suma = 0, promedio;
+    
+
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Ingresa Calificación "+i);
+            calificacion = read.nextFloat();
+            suma =+ calificacion;
+        }
+
+        promedio = (suma / 3);
+        System.out.println("EL promedio es: "+ promedio);
+
 
         
+        
+
+
     }
 }
